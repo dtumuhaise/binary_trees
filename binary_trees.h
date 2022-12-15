a7aa03a87a05aae3f580112b2d97b23c9964fb2d
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-15 11:33:52 +0300

    1. Insert left

--- binary_trees.h
+++ binary_trees.h
@@ -6,7 +6,7 @@
 
 /**
  * struct binary_tree_s - Binary tree node
- *
+
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -38,6 +38,8 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 
 /* prototypes*/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
