bec17371b265b4aac4794040817231bbe0fe4a3f
Davis Tumuhaise <tumuhaise@gmail.com>
2022-12-15 11:48:36 +0300

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
 		parent->right->parent = newNode;
